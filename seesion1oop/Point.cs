540cb52adc45d40faec58ce24ee87b0ea257dee5
Greg Low <greg@gmail.com>
2024-12-31 16:14:46 +0200

    STRUCT

---
+++ seesion1oop/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seesion1oop
+{
+    internal struct Point
+    {
+        #region STRUCT
+        //public int x; 
+        //public int y;
+        //Constructor: special methods
+        //1.Named like struct 
+        //1.has return type 
+        //clr:will generate parameterless constructor 
+        //this constructor will initialized the attributes with the default value 
+
+        //construcor
+        //public Point()
+        //{
+        //}
+        //public Point()
+        //{
+        //    x = 8;
+        //    y = 5;
+        //} 
+        #endregion
+
+
+
+    }
+}
--- seesion1oop/Program.cs
+++ seesion1oop/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace seesion1oop
+﻿using System.Security.Cryptography;
+
+namespace seesion1oop
 {
     internal class Program
     {
+        //class
+        //struct
+        //interface
+        //enum
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region STRUCT
+            //struct : valuetype  store in stack
+            // Employee
+            //ID // NAME // SALARY
+            // point 
+           // Point PO1;
+            ////Declare for object from type 'point'
+            ////po1 : object 
+            ////allocate untitialized 8 bytes at stack 
+            ////Console.WriteLine(PO1.X);
+            ////Console.WriteLine(PO1.Y);
+            //PO1.x = 10;
+            //PO1.y = 15;
+            //Console.WriteLine(PO1.x);
+            //Console.WriteLine(PO1.y);
+            //PO1 = new Point();
+            //NEW USED FOR SELECT THE CONSTRUCTOR
+
+            //Console.WriteLine(PO1.x);
+            //Console.WriteLine(PO1.y); 
+            #endregion
+
+
         }
     }
 }
