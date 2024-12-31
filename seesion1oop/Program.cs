6182f32eb9b2760bd703cb6d82e92a5e372720a6
Greg Low <greg@gmail.com>
2024-12-31 14:02:54 +0200

    Add project files.

---
+++ seesion1oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace seesion1oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
